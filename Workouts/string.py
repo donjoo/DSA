abc7e53cf11408369d179d40ccfbd5add0a9757c
donjoo <donjorois@gmail.com>
2025-07-13 18:07:06 +0530

    added string workout

---
+++ Workouts/string.py
@@ -0,0 +1,27 @@
+# Check if Two Strings Are Anagrams (Without sorted())
+# Task: Return True if one string is an anagram of the other.
+# # Input: "listen", "silent"
+# # Output: True
+# Twist: Account for spaces and case insensitivity.
+def ana(str1,str2):
+    str1 = str1.replace(" ","").lower()
+    str2 = str2.replace(" ","").lower()
+    
+    if len(str1) != len(str2):
+        return False
+        
+    str1_map = {}
+    str2_map = {}
+    
+    for char in str1:
+        str1_map[char] = str1_map.get(char,0) +1 
+    for char in str2:
+        str2_map[char] = str2_map.get(char,0) +1
+
+    return str1_map == str2_map
+    
+print(ana("listen", "silEnt"))
+
+
+
+# Replace all occurrences of a substring old in string text with a new substring new.
