ebae24a21996ab0e0eda15aa3e7f67dd942db8ca
donjoo <donjorois@gmail.com>
2025-07-21 00:25:01 +0530

    added hash table-linkedlist version

--- week_1/Hash/hash.py
+++ week_1/Hash/hash.py
@@ -202,7 +202,7 @@ class HashTable:
         for k, v in self.table[idx]:
             if k == key:
                 return v
-        return None
+        return None 
 
     def delete(self, key):
         idx = self._hash(key)
@@ -216,3 +216,82 @@ class HashTable:
 
 
 
+class Node:
+    def __init__(self,key,value):
+        self.key = key
+        self.value = value
+        self.next = None
+
+
+
+class HashTable:
+    
+    def __init__(self,size=10):
+        self.size = size
+        self.table = [None] * size
+        
+    def _hash(self,key):
+        return hash(key) % self.size
+        
+    def insert(self,key,value):
+        index = self._hash(key)
+        head = self.table[index]
+        while head:
+            if head.key == key:
+                head.value = value
+                return True
+            head = head.next
+        
+        new_node = Node(key,value)
+        new_node.next = self.table[index]
+        self.table[index] = new_node
+        
+        
+    def get(self,key):
+        index = self._hash(key)
+        current = self.table[index]
+        while current:
+            if current.key == key:
+                return current.value
+            current = current.next
+            
+            
+    def delete(self,key):
+        index = self._hash(key)
+        current = self.table[index]
+        prev = None
+        
+        while current:
+            if current.key == key:
+                if prev:
+                    prev.next = current.next
+                else:
+                    self.table[index] = current.next
+                return True
+            prev = current
+            current = current.next
+        return False
+        
+    
+    def display(self):
+        for i,node in enumerate(self.table):
+            print(f"{i}", end=" ")
+            current = node
+            while current:
+                print(f"({current.key}:{current.value})", end =" ->")
+                current =current.next
+            print('None')
+            
+            
+            
+ht = HashTable()
+ht.insert("name", "Alice")
+ht.insert("age", 25)
+ht.insert("gender", "female")
+ht.insert("name", "Bob")  # Updates
+
+print(ht.get("age"))      # 25
+ht.delete("gender")
+ht.display()
+
+            
