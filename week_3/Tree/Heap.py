0090a869aee5215958014598368acb094b07e28b
donjoo <donjorois@gmail.com>
2025-08-02 00:37:44 +0530

    min heap

--- week_3/Tree/Heap.py
+++ week_3/Tree/Heap.py
@@ -65,3 +65,50 @@ Use Case	                        Why Heap is Used
 
 
 '''
+
+
+class MinHeap:
+    def __init__(self):
+        self.heap = []
+
+    def insert(self, val):
+        self.heap.append(val)
+        self._heapify_up(len(self.heap) - 1)
+
+    def extract_min(self):
+        if len(self.heap) == 0:
+            return None
+        if len(self.heap) == 1:
+            return self.heap.pop()
+
+        root = self.heap[0]
+        self.heap[0] = self.heap.pop()
+        self._heapify_down(0)
+        return root
+
+    def _heapify_up(self, i):
+        while i > 0:
+            parent = (i - 1) // 2
+            if self.heap[i] < self.heap[parent]:
+                self.heap[i], self.heap[parent] = self.heap[parent], self.heap[i]
+                i = parent
+            else:
+                break
+
+    def _heapify_down(self, i):
+        n = len(self.heap)
+        while True:
+            smallest = i
+            left = 2*i + 1
+            right = 2*i + 2
+            
+            if left < n and self.heap[left] < self.heap[smallest]:
+                smallest = left
+            if right < n and self.heap[right] < self.heap[smallest]:
+                smallest = right
+            
+            if smallest != i:
+                self.heap[i], self.heap[smallest] = self.heap[smallest], self.heap[i]
+                i = smallest
+            else:
+                break
